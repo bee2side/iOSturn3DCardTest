a7451e730b4906e2c44b6c1d2a31c01184d33b1d
dylan.k <bee2side@gmail.com>
2020-07-23 09:08:20 +0900

    turn animation

--- iOSturn3DCardTest/ViewController.swift
+++ iOSturn3DCardTest/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var btnCard: UIButton!
     var transform = CATransform3DIdentity
+    var transDegree: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let rotation = CATransform3DMakeRotation(CGFloat(30.0 * Double.pi / 180.0), 0, 0, 0)
-        //self.btnCard.layer.transform = CATransform3DTranslate(rotation, 0, 20, -30)
         transform.m34 = 1.0 / 500.0
+        transDegree = 0
         transform = CATransform3DRotate(transform, CGFloat(45 * Double.pi / 180), 0, 1, 0)
-        btnCard.layer.transform = transform
+//        btnCard.layer.transform = transform
     }
 
     @IBAction func btnFlip(_ sender: Any) {
@@ -30,14 +30,20 @@ class ViewController: UIViewController {
             isOpen = false
             let cardImage = UIImage(named: "kakaounit_front")
             btnCard.setImage(cardImage, for: .normal)
+            transDegree = 90
+            self.transform = CATransform3DRotate(self.transform, CGFloat(self.transDegree * Double.pi / 180), 0, 1, 0)
             UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: .calculationModeCubic, animations: {
-//                self.btnCard.layer.transform = CATransform3DTranslate(rotation, 0, 0, 0)
+                self.btnCard.layer.transform = self.transform
             }, completion: nil)
         } else {
             isOpen = true
             let cardImage = UIImage(named: "kakaounit_back")
             btnCard.setImage(cardImage, for: .normal)
-            UIView.transition(with: btnCard, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
+            transDegree = 180
+            self.transform = CATransform3DRotate(self.transform, CGFloat(self.transDegree * Double.pi / 180), 0, 1, 0)
+            UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: .calculationModeCubic, animations: {
+                self.btnCard.layer.transform = self.transform
+            }, completion: nil)
         }
     }
     
